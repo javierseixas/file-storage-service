b429765b45186b49f4d8d95d05afac41866e7c40
Javier Seixas <dev@javierseixas.com>
2018-05-24 14:44:55 -0400

    Initial commit

---
+++ src/main/java/com/javierseixas/test/filerest/FileRestApplication.java
@@ -0,0 +1,12 @@
+package com.javierseixas.test.filerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileRestApplication.class, args);
+	}
+}
