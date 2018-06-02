eb347c4b4e1f9d03ad3d1b4d2bdc42f193944924
Javier Seixas <dev@javierseixas.com>
2018-06-02 11:58:26 -0400

    Adds file data fields in controller

--- src/main/java/com/javierseixas/test/filerest/infrastructure/controller/FileController.java
+++ src/main/java/com/javierseixas/test/filerest/infrastructure/controller/FileController.java
@@ -4,15 +4,13 @@ import com.javierseixas.test.filerest.domain.File;
 import com.javierseixas.test.filerest.domain.FileRepository;
 import com.javierseixas.test.filerest.infrastructure.storage.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Date;
 
-@Controller
+@RestController
 public class FileController {
 
     private FileRepository fileRepository;
@@ -25,23 +23,18 @@ public class FileController {
     }
 
     @PostMapping("/files")
-    public String handleFileUpload(
+    public File handleFileUpload(
             @RequestParam("file") MultipartFile file,
+            @RequestParam("name") String name,
+            @RequestParam("description") String description,
             RedirectAttributes redirectAttributes
     ) {
 
-        File filee = new File("id", "name", "description", new Date());
+        File fileRecord = new File(name, description, new Date());
 
         storageService.store(file);
-        fileRepository.add(filee);
-        redirectAttributes.addFlashAttribute("message",
-                "You successfully uploaded " + file.getOriginalFilename() + "!");
+        fileRepository.add(fileRecord);
 
-        return "redirect:/";
+        return fileRecord;
     }
-
-//    @ExceptionHandler(StorageFileNotFoundException.class)
-//    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
-//        return ResponseEntity.notFound().build();
-//    }
 }
