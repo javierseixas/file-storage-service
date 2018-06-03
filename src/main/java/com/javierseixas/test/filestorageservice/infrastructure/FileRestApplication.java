c98dacd28fc4a623b1f3c52b6e7bd44d47b6ffcf
Javier Seixas <dev@javierseixas.com>
2018-06-03 12:47:19 -0400

    Allows CORS

--- src/main/java/com/javierseixas/test/filestorageservice/infrastructure/FileRestApplication.java
+++ src/main/java/com/javierseixas/test/filestorageservice/infrastructure/FileRestApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
@@ -23,6 +26,16 @@ public class FileRestApplication {
         return (args) -> {
             storageService.deleteAll();
             storageService.init();
+        };
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/files").allowedOrigins("http://localhost:3000");
+            }
         };
     }
 }
